08d45ea69b71efb584cb5420492524ca8fb9ae47
Andrew Peterson <peterson8000@yahoo.com>
2020-03-25 23:27:44 -0700

    update docs with Rules endpoints

--- server/tests/index.js
+++ server/tests/index.js
@@ -7,71 +7,80 @@ const _ = require('lodash');
 chai.use(chaiHttp);
 chai.should();
 
+before('clearDb of data', async () => {
+  await Promise.all(
+    _.map(Object.keys(models), (key) => {
+      if (['sequelize', 'Sequelize'].includes(key)) return null;
+      console.log(key + ' cleared!');
+      return models[key].destroy({ truncate : true, restartIdentity: true, cascade: true });
+    })
+  );
+});
 
-// describe("regional shipping program eligibility tests", async () => {
-//
-//   describe("eligibility calls", async () => {
-//
-//     // it("should return not eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//     //
-//     // it("should return not eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//     //
-//     // it("should return not eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//     //
-//     // it("should return eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//     //
-//     // it("should return eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//     //
-//     // it("should return eligible", (done) => {
-//     //   chai.request(app)
-//     //     .get('/api/rules')
-//     //     .end((err, res) => {
-//     //       res.should.have.status(200);
-//     //       res.body.should.be.a('array');
-//     //     done();
-//     //   });
-//     // });
-//
-//   });
-//
-// });
+describe("regional shipping program eligibility test", async () => {
+
+  describe("seed", async () => {
+
+    // it("should return not eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+    //
+    // it("should return not eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+    //
+    // it("should return not eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+    //
+    // it("should return eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+    //
+    // it("should return eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+    //
+    // it("should return eligible", (done) => {
+    //   chai.request(app)
+    //     .get('/api/rules')
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('array');
+    //     done();
+    //   });
+    // });
+
+  });
+
+});
